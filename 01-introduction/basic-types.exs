b143c21db3ea623a1ff00808a242d961a4628207
Leo Baiano <leo.baiano@copilotu.ai>
2024-01-15 09:03:10 -0300

    add baic types

---
+++ 01-introduction/basic-types.exs
@@ -0,0 +1,23 @@
+# integer
+_ = 1
+
+# integer
+_ = 0x1F
+
+# float
+_ = 1.0
+
+# boolean
+_ = true
+
+# atom / symbol
+_ = :atom
+
+# string
+_ = "elixir"
+
+# list
+_ = [1, 2, 3]
+
+# tuple
+_ = {1, 2, 3}
